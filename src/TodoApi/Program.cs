5b8820594d9f7395a9544b9a7d673a013d02ddab
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2023-04-26 10:21:08 +0200

    finished api endpoints

--- src/TodoApi/Program.cs
+++ src/TodoApi/Program.cs
@@ -17,7 +17,59 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{builder.Environment.ApplicationName} v1"));
 
-// API endpoints
+// Get All Todos
 app.MapGet("/todos", async (TodoDb db) => await db.Todos.ToListAsync());
+
+// Get Todo by ID
+app.MapGet("/todo/{id:guid}", async (Guid id, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+    return todo == null ? Results.NotFound() : Results.Ok(todo);
+});
+
+// Get complete todos
+app.MapGet("/todos/complete", async (TodoDb db) => await db.Todos.Where(todo => todo.IsComplete).ToListAsync());
+
+// Create todo
+app.MapPost("/todo", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todo/{todo.Id}", todo);
+});
+
+// Update Todo
+app.MapPut("/todo/{id:guid}", async (Guid id, Todo update, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo == null)
+    {
+        return Results.NotFound();
+    }
+
+    todo.Title = update.Title ? update.Title : todo.Title;
+    todo.IsComplete = update.IsComplete ;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+// Delete Todo
+app.MapDelete("/todo/{id:guid}", async (Guid id, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo == null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Todos.Remove(todo);
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
 
 app.Run();
